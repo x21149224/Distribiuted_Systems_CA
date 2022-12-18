e3970178e6c36868a12b7c485605fd2b6c12dd51
Kasia <92877823+x21149224@users.noreply.github.com>
2022-12-18 19:05:31 +0000

    client

--- app.js
+++ app.js
@@ -1,163 +1,154 @@
-//q1 area_pollution//
+//Server1 area_pollution//
+var readlineSync = require('readline-sync')
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
 var PROTO_PATH = __dirname + "/protos/area_pollution.proto"
+
 var packageDefinition = protoLoader.loadSync(PROTO_PATH)
-var area_pollution_proto = grpc.loadPackageDefinition(packageDefinition).area_pollution
+var area_pollution = grpc.loadPackageDefinition(packageDefinition).area_pollution
+var client = new area_pollution.RandomService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
-function generateRandomComponent(call, callback){
-	for(var i=0; i<call.request.amount; i++){
+var action = readlineSync.question(
+"What would you like to do?\n"
++" \t 1 Check the pollution in the area\n"
++ "\t 2 Compare the pollution in different cities\n"
++ "\t 3 Check the ranking of most polluted cities\n"
++ "\t 4 Check the average pollution in different cities\n")
 
-	call.write({
-		value: Math.round(Math.random() * 100)
+action = parseInt(action)
+
+if(action ===1){
+var amount = parseInt(readlineSync.question("How many cities would you like to see the components of the smog for?"))
+var call = client.generateRandomComponent({amount: amount})
+
+call.on('data', function(response){
+	console.log("\nIn requested city. \nThe PM2.5 level is: " + response.value + "\nThe PM10 level is: " + (response.value+15) + 
+    "\nThe CO level is: " + (response.value+23) + 
+    "\nThe SO2 level is: " + (response.value+29) + "\nThe C6H6 level is: " + response.value + 
+    "\nThe O3 level is: " + (response.value+5) + "\nThe NO2 level is: " + (response.value+31)  )
+    
+})
+ 	
+	 call.on('end', function(){
 	})
-	}
-	call.end()
-}
+	
+	call.on('error', function(e){
+	console.log(e)
+	console.log("An error occured")
+	})  
 
 
-
-
-//q2 compare_pollution//
+//Server2 compare_pollution//
+}else if(action ===2){
+var readlineSync = require('readline-sync')
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
 var PROTO_PATH = __dirname + "/protos/compare_pollution.proto"
+
 var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var compare_pollution_proto = grpc.loadPackageDefinition(packageDefinition).compare_pollution
-
-
-function subtract(call, callback){
-  try{
-  var city1 = parseInt(call.request.city1)
-  var city2 = parseInt(call.request.city2)
-  if(city1 > 6 || city2 > 6 || city1===0 || city2===0 ) {
-    callback(null, {
-      message: "There is no such a city on the list, please choose beween 1-6"
-    })
-    }
-  
- else if(!isNaN(city1) && !isNaN(city2)){
-    var result = city2 - city1
-    console.log(result)
-    callback(null, {
-      message:undefined,
-      result: result
-     })
-  } else{
-    callback(null, {
-      message: "Please provide two cities"
-    })
-  }
-}catch(e){
-  callback(null, {
-    message: "An error occured during computation"
-  })
-}
-
-}
-
-
-function divide(call, callback) {
+var client = new compare_pollution_proto.CalcService("0.0.0.0:40000", grpc.credentials.createInsecure());
+var monitor = readlineSync.question(
+  "What would you like to monitor?\n"
+  + "\t 1 The difference in pollution between two cities\n"
+  + "\t 2 How many times one city is more polluted than the other one\n"
+)
+monitor = parseInt(monitor)
+if(monitor === 1) {
+  var city1 = readlineSync.question("What is the first city, provide a number:\n 1 Dublin\n 2 Galway\n 3 Cork\n 4 Limerick\n 5 Waterford\n 6 Sligo\n ")
+  var city2 = readlineSync.question("What is the second city, provide a number:\n 1 Dublin\n 2 Galway\n 3 Cork\n 4 Limerick\n 5 Waterford\n 6 Sligo\n ")
   try {
-    var city1 = parseInt(call.request.city1)
-    var city2 = parseInt(call.request.city2)
-    if(city2 === 0 || city1===0 || city1>6 || city2>6 ) {
-      callback(null, {
-        message: "There is no such a city on the list please choose between 1-6"
-      })
-    }
-    else if(!isNaN(city1) && !isNaN(city2)) {
-      var result = city2 / city1
-      callback(null, {
-        message: undefined,
-        result: result
-      })
-    } else {
-      callback(null, {
-        message: "Please provide two cities"
-      })
-
-    }
+    client.subtract({city1: city1, city2: city2}, function(error, response) {
+      try {
+        if(response.message) {
+          console.log(response.message)
+        } else {
+          console.log("First city is of" + response.result + " AAQI more polluted than second city.")
+        }
+      } catch(e) {
+        console.log("Could not connect to server")
+      }
+    })
 
   } catch(e) {
-    callback(null, {
-      message: "An error occured during computation"
-    })
-
+    console.log("An error occured")
   }
 
+} else if(monitor === 2) {
+	var city1 = readlineSync.question("What is the first city, provide a number:\n 1 Dublin\n 2 Galway\n 3 Cork\n 4 Limerick\n 5 Waterford\n 6 Sligo\n ")
+  	var city2 = readlineSync.question("What is the second city, provide a number:\n 1 Dublin\n 2 Galway\n 3 Cork\n 4 Limerick\n 5 Waterford\n 6 Sligo\n ")
+    try {
+      client.divide({city1: city1, city2: city2}, function(error, response) {
+        try {
+          if(response.message) {
+            console.log(response.message)
+          } else {
+            console.log("First city is " + response.result + " times more polluted than second city.")
+          }
+        } catch(e) {
+          console.log("Could not connect to server")
+        }
+      })
+    } catch(e) {
+      console.log("An error occured")
+    }
+} else {
+  console.log("Error: Operation not recognized")
 }
 
-
-
-
-//q3 ranking//
+//Server3 ranking//
+}else if( action===3){
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
 var PROTO_PATH = __dirname + "/protos/ranking.proto"
-var packageDefinition = protoLoader.loadSync(PROTO_PATH)
-var ranking_proto = grpc.loadPackageDefinition(packageDefinition).ranking
 
-var data = [
-  {placeRanking: 1, cityName:"Dublin", pollutionLevel: 55},
-  {placeRanking: 2, cityName:"Cork", pollutionLevel: 41},
-  {placeRanking: 3, cityName:"Sligo", pollutionLevel: 37},
-  {placeRanking: 4, cityName:"Limerick", pollutionLevel: 29},
-  {placeRanking: 5, cityName:"Galway", pollutionLevel: 25},
-  {placeRanking: 6, cityName:"Waterford", pollutionLevel: 22},
-  {placeRanking: 7, cityName:"Wexford", pollutionLevel: 19},
-  {placeRanking: 8, cityName:"Ballina", pollutionLevel: 18},
-  {placeRanking: 9, cityName:"Carlow", pollutionLevel: 17},
-  {placeRanking: 10, cityName:"Athlone", pollutionLevel: 12}]
+var packageDefinition = protoLoader.loadSync(PROTO_PATH);
+var ranking= grpc.loadPackageDefinition(packageDefinition).ranking;
+var client = new ranking.RankingService('localhost:40000', grpc.credentials.createInsecure());
+var call = client.getCityRanking({ });
 
-function getCityRanking(call, callback) {
-  for(var i=0; i<data.length; i++){
-    call.write({
-      placeRanking: data[i].placeRanking,
-      cityName: data[i].cityName,
-      pollutionLevel: data[i].pollutionLevel
-    })
-  }
-  call.end()
-}
+call.on('data', function (response){
+  console.log(response.placeRanking + " most polluted city in Ireland is " + response.cityName + " with " + response.pollutionLevel + " AAQI level" )
+});
+
+call.on('end', function(){
+});
+
+call.on('error', function(e){
+  console.log(e)
+});
 
 
-
-
-//q4 average//
+//Server4 average//
+}else if(action === 4){
+var readlineSync = require('readline-sync')
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
 var PROTO_PATH = __dirname + "/protos/average.proto"
+
 var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var average_proto = grpc.loadPackageDefinition(packageDefinition).average
+var client = new average_proto.AverageService("0.0.0.0:40000", grpc.credentials.createInsecure());
+var call = client.calcAverage(function(error, response){
+  if(error){
+    console.log("An error occured")
+  }else{
+    console.log("The average pollution of " + response.cities + " cities, is: " + response.average)
+  }
+})
 
-function calcAverage(call, callback) {
-  var cities = 0
-  var sum = 0
 
-  call.on('data', function(request){
-    sum += request.level
-    cities += 1
-    
-  })
-  
-    call.on("end", function(){
-      callback(null,{
-        average: sum/cities,
-	     cities: cities
-        
-      })
-    })
-  
-  call.on('error', function(e){
-	console.log("An error occured")
-  })
+while (true){
+  var city_name = readlineSync.question("What is the name of the city? (q to Quit):")
+  if(city_name.toLowerCase() === "q"){
+    break
+  }
+  var level = readlineSync.question("What is the pollution level in this city?")
+
+  call.write({level: parseFloat(level),  cityname: city_name})
 }
 
-var server = new grpc.Server()
-server.addService(area_pollution_proto.RandomService.service,{generateRandomComponent: generateRandomComponent})
-server.addService(compare_pollution_proto.CalcService.service, {subtract: subtract, divide: divide})
-server.addService(ranking_proto.RankingService.service, {getCityRanking: getCityRanking})
-server.addService(average_proto.AverageService.service, {calcAverage: calcAverage})
-server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() { server.start()
-})
+call.end()
+
+
+}
